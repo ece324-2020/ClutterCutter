9a731701243801e88b333bcac75ae1d6ae485b2c
Cindy Hu <57768178+tianxic-hu@users.noreply.github.com>
2020-11-22 16:59:52 -0500

    Update utils.py
    Added function decontracted to decontract words. For example, '"it's" into "it is".

--- utils.py
+++ utils.py
@@ -3,6 +3,23 @@ import pandas as pd
 import csv
 from sklearn.model_selection import train_test_split
 import string
+import re 
+
+def decontracted(phrase):
+    # specific
+    phrase = re.sub(r"won\'t", "will not", phrase)
+    phrase = re.sub(r"can\'t", "can not", phrase)
+
+    # general
+    phrase = re.sub(r"n\'t", " not", phrase)
+    phrase = re.sub(r"\'re", " are", phrase)
+    phrase = re.sub(r"\'s", " is", phrase)
+    phrase = re.sub(r"\'d", " would", phrase)
+    phrase = re.sub(r"\'ll", " will", phrase)
+    phrase = re.sub(r"\'t", " not", phrase)
+    phrase = re.sub(r"\'ve", " have", phrase)
+    phrase = re.sub(r"\'m", " am", phrase)
+    return phrase
 
 def reformat_txt(path):
     """
