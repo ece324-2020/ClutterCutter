146f244d53006eeaee70788e00d869b7e3cad034
Cindy Hu <57768178+tianxic-hu@users.noreply.github.com>
2020-12-06 20:44:28 -0500

    list words by frequency in each email category

--- word_freq.py
+++ word_freq.py
@@ -1,1 +1,14 @@
+import re
+from collections import Counter
 
+filename = 'personal.tsv'
+with open(filename) as f:
+    passage = f.read()
+
+words = re.findall(r'\w+', passage)
+
+cap_words = [word.lower() for word in words if len(word)>4 and word not in ['there','these']]
+
+word_counts = Counter(cap_words)
+
+print(word_counts)
